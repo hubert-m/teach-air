86357d8c305016b9a64408384c4215a4d1ca2575
Hubert Machała <hubert.machala@o2.pl>
2021-10-20 03:29:01 +0200

    Add messages, options and courses tables
    Dodano 3 migracje tabel:
    messages, options, courses

---
+++ backend/database/migrations/2021_10_20_003809_create_messages.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('sender_id')->unsigned()->index();
+            $table->bigInteger('recipient_id')->unsigned()->index();
+            $table->text("content");
+            $table->tinyInteger("is_read")->default(0); // jeśli przeczytana wiadomość to 1
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+        Schema::table('messages', function($table) {
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('recipient_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
---
+++ backend/database/migrations/2021_10_20_011509_create_courses.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 255);
+            $table->text("description")->nullable();
+            $table->string("icon", 200)->nullable(); // path to image or font awesome code
+            $table->string("slug", 200); // use in uri address
+            $table->bigInteger('parent_id')->unsigned()->index()->nullable(); // jesli NULL to kurs glowny, nadrzedny
+            $table->bigInteger('created_by')->unsigned()->index();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+        Schema::table('courses', function($table) {
+            $table->foreign('parent_id')->references('id')->on('courses');
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
