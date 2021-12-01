aca9c767ac532becb658df2d9cc524f7bc14ae05
Hubert Machała <hubert.machala@o2.pl>
2021-12-01 19:11:17 +0100

    Separate listOfMembers from formAddMember

--- frontend/web/src/views/Courses/components/FormAddMember.js
+++ frontend/web/src/views/Courses/components/FormAddMember.js
@@ -8,6 +8,7 @@ import {faTrash, faUserPlus} from "@fortawesome/free-solid-svg-icons";
 import {sortDesc} from "../../../helpers/sort";
 import {addMember, deleteMember, getMembersOfCourse} from "../../../helpers/Course";
 import SweetAlert from "react-bootstrap-sweetalert";
+import ListOfMembers from "./ListOfMembers";
 
 const FormAddMember = ({courseId}) => {
     const [loadOptions, setLoadOptions] = useState([]);
@@ -180,76 +181,8 @@ const FormAddMember = ({courseId}) => {
                     </tbody>
                 </table>
             )}
+            <ListOfMembers members={members} handleDeleteMember={handleDeleteMember} />
 
-            <div className="jumbotron" style={{marginTop: '50px'}}>
-                <h2>Lista członków</h2>
-                <hr className="my-4"/>
-            </div>
-
-            {isEmpty(members) ? (<p>Brak</p>) : (
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Imię</th>
-                        <th scope="col">Nazwisko</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col" colSpan={2}>&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {members.map(({
-                                      id,
-                                      email,
-                                      name,
-                                      second_name,
-                                      lastname,
-                                      is_author,
-                                      is_author_or_member_of_one_of_parent,
-                                      status
-                                  }) => (
-
-                        <tr key={id}>
-                            <th scope="row">{id}</th>
-                            <td>{name} {second_name}</td>
-                            <td>{lastname}</td>
-                            <td>{email}</td>
-                            {status === StatusUser.ADMIN ?
-                                (
-                                    <td><span className="badge bg-danger">{StatusUserName[StatusUser.ADMIN]}</span>
-                                    </td>
-                                ) : status === StatusUser.UNACTIVATED ?
-                                    (
-                                        <td><span
-                                            className="badge bg-secondary">{StatusUserName[StatusUser.UNACTIVATED]}</span>
-                                        </td>
-                                    ) : status === StatusUser.TEACHER ?
-                                        (
-                                            <td><span
-                                                className="badge bg-warning">{StatusUserName[StatusUser.TEACHER]}</span>
-                                            </td>
-                                        ) : (
-                                            <td><span
-                                                className="badge bg-primary">{StatusUserName[StatusUser.STUDENT]}</span>
-                                            </td>
-                                        )
-                            }
-                            <td>{is_author === 1 ? (
-                                <span className="badge bg-danger">Autor tego kursu</span>
-                            ) : is_author_or_member_of_one_of_parent === 1 ? (
-                                <span className="badge bg-danger">Członek lub autor jednego z kursów nadrzędnych</span>
-                            ) : (
-                                <button type="button" className="btn btn-danger"
-                                        onClick={() => handleDeleteMember(id)}><FontAwesomeIcon
-                                    icon={faTrash}/>
-                                </button>
-                            )
-                            }</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
             <SweetAlert
                 error
                 show={showError}
